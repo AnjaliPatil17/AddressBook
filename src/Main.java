6c3c84f48df2891bc372e6783bde86045e85e2c2
Anjali <anjaliap1227@gmail.com>
2024-05-24 11:38:30 +0530

    Address_Book_UC3-Edit_Contact file add

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,37 @@
+import java.util.Scanner;
 
-import java.util.*;
 public class Main {
+    public static boolean exit = true;
+
+    public static void options() {
+        System.out.println("(1) Create New Contact\n(2) Edit Contact\n(3) Display\n(4) Exit");
+        Scanner sc = new Scanner(System.in);
+        Contact obj = new Contact();
+        int option = sc.nextInt();
+
+        switch (option) {
+            case 1:
+                obj.addContact();
+                break;
+            case 2:
+                obj.updateContact();
+                break;
+            case 3:
+                System.out.println(obj.getInfo());
+                break;
+            case 4:
+                exit = false;
+                break;
+            default:
+                System.out.println("Invalid option.");
+                break;
+        }
+    }
+
     public static void main(String[] args) {
-        Boolean exit = true;
-        while(exit){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("******Welcome to Address Book*******");
-            Contact Swapnil = new Contact();
-            System.out.println("(1) Create New Contact\n(2) Edit Contact\n(3) Display Contact\n(4)Exit");
-            int option = sc.nextInt();
-            switch(option){
-                case 1:
-                    Swapnil.addContact();
-                    ;
-                case 2:
-                    Swapnil.updateContact();
-                    ;
-                case 3:
-                    System.out.println(Swapnil.getInfo());
-                    ;
-                case 4:
-                    exit = false;
-            }
+        System.out.println("******Welcome to Address Book*******");
+        while (exit) {
+            options();
         }
     }
 }
